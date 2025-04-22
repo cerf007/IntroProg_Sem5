e71cc271589185249f306e11147a2abffdb99ca3
cerf007 <cerf02062007@gmail.com>
2025-04-22 07:32:01 -0600

    Calculo de promedio

---
+++ actividad3/ejercicio1.py
@@ -0,0 +1,12 @@
+calificacion1 = int(input("Ingrese la calificación 1: "))
+calificacion2 = int(input("Ingrese la calificación 2: "))
+calificacion3 = int(input("Ingrese la calificación 3: "))
+suma = calificacion1 + calificacion2 + calificacion3
+promedio = suma / 3
+print(f"""Calificación 1: {calificacion1}
+ Calificación 2: {calificacion2} 
+ Califiación 3: {calificacion3}
+ Promedio: {promedio:.2f}
+ Promedio: {promedio:.0f}""")
+
+
