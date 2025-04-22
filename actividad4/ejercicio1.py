e3efc9fd0acfbcd73332d9fe39663e5088045d22
cerf007 <cerf02062007@gmail.com>
2025-04-22 11:23:02 -0600

    Ejercicio de numero valido o invalido

---
+++ actividad4/ejercicio1.py
@@ -0,0 +1,8 @@
+inicio = int(input("Dime el valor inicial: "))
+fin = int (input("Dime el valor final: "))
+
+numero = int(input("Dime un numero: "))
+if numero >= inicio and numero <= fin:
+    print("El número es válido")
+else:
+    print("El número es invalido")
