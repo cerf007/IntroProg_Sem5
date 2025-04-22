fdae91c917bb58fd208660f851937cf2af602cd8
cerf007 <cerf02062007@gmail.com>
2025-04-22 07:50:53 -0600

    Se arreglo mis errores

--- actividad3/ejercicio1.py
+++ actividad3/ejercicio1.py
@@ -4,9 +4,8 @@ calificacion3 = int(input("Ingrese la calificación 3: "))
 suma = calificacion1 + calificacion2 + calificacion3
 promedio = suma / 3
 print(f"""Calificación 1: {calificacion1:>3}
- Calificación 2: {calificacion2:>3} 
- Calificación 3: {calificacion3:>3}
- Promedio: {promedio:.2f}
- {"Promedio":<15} {promedio:>3.0f}""")
+Calificación 2: {calificacion2:>3} 
+Calificación 3: {calificacion3:>3}
+{"Promedio":<15} {promedio:>3.0f}""")
 
 
