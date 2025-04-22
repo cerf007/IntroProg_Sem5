82c94156980b6d9fe92f2491c66a3505bd31c8ac
cerf007 <cerf02062007@gmail.com>
2025-04-22 07:47:54 -0600

    Se realizo u formato a las salida

--- actividad3/ejercicio1.py
+++ actividad3/ejercicio1.py
@@ -3,10 +3,10 @@ calificacion2 = int(input("Ingrese la calificación 2: "))
 calificacion3 = int(input("Ingrese la calificación 3: "))
 suma = calificacion1 + calificacion2 + calificacion3
 promedio = suma / 3
-print(f"""Calificación 1: {calificacion1}
- Calificación 2: {calificacion2} 
- Califiación 3: {calificacion3}
+print(f"""Calificación 1: {calificacion1:>3}
+ Calificación 2: {calificacion2:>3} 
+ Calificación 3: {calificacion3:>3}
  Promedio: {promedio:.2f}
- Promedio: {promedio:.0f}""")
+ {"Promedio":<15} {promedio:>3.0f}""")
 
 
