e9e9fdcec01681cbe01929e89e192a0b0874d59e
cerf007 <cerf02062007@gmail.com>
2025-04-28 19:27:18 -0600

    Ejercicio de los días

---
+++ sesion_viernes/ejercicio3.py
@@ -0,0 +1,16 @@
+dia = int(input("Ingres el dia de la semana (1-7: )"))
+
+if dia ==1:
+    print("Lunes")
+elif dia==2:
+    print("Martes")
+elif dia==3:
+    print("Miércoles")
+elif dia ==4:
+    print("Jueves")
+elif dia==5:
+    print("Viernes")
+elif dia==6:
+    print("Sábado")
+elif dia==7:
+    print("Domingo")
